e0ff5d2526ddaa17499db20579b73f1545b23f0d
nathan-park25 <73155685+nathan-park25@users.noreply.github.com>
2021-04-16 07:33:51 +1000

    Update handling_dynamics.py

--- psspy/handling_dynamics.py
+++ psspy/handling_dynamics.py
@@ -19,3 +19,13 @@ Wanted value can be derived by modifying value of aux_con0, var0 and icon0
 _,aux_con_value=psspy.dsrval('CON',aux_con0)
 _,aux_var_value=psspy.dsrval('VAR',aux_var0)
 _,aux_icon_value=psspy.dsival('ICON',aux_icon0)
+
+## CHANGE ICON, CON, VAR DURING DYNAMICS ###
+## Change STR
+ierr=psspy.change_wnmod_chricn(gens[0][0],gens[0][1],aux_name,icon_num,'STR_CHANGE') ## icon_num to change relative to the start of the block (1 for ICON(I), 2 for ICON(I+1))
+## CHANGE ICON
+ierr=psspy.change_wnmod_icon(gens[0][0],gens[0][1],aux_name,icon_num,value) ## icon_num to change relative to the start of the block (1 for ICON(I), 2 for ICON(I+1))
+## CHANGE CON
+ierr=psspy.change_wnmod_icon(gens[0][0],gens[0][1],aux_name,icon_num,value) ## icon_num to change relative to the start of the block (1 for CON(J), 2 for CON(J+1))
+## CHANGE VAR
+ierr=psspy.change_wnmod_var(gens[0][0],gens[0][1],aux_name,icon_num,'STR_CHANGE') ## icon_num to change relative to the start of the block (1 for VAR(J), 2 for VAR(J+1))
